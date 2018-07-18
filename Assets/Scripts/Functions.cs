50ce390fed1d97e97ce8efe5e9f4effe4d7465c2
Thales Bueno Pasqual <thales.pasqual@gmail.com>
2018-07-18 17:07:02 -0300

    Implementação da classe Function Implementação e documentação da função PersonAnalize

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -16,14 +16,14 @@ public class Functions : MonoBehaviour {
         Vector2[] movements;
         Vector2[] borderLine;
         char tab = Convert.ToChar("\t");
-
-        // Movement space variables
-        // Movement space variables
+        
+        // Movement space variables (ellipse parameters)
         Vector2 min, max;
         Vector2 center, bases;
+        Vector2 polar, offset;
 
-    // Initialize borderline variable with a defined resolution
-    borderLine = new Vector2[36];
+        // Initialize borderline variable with a defined resolution
+        borderLine = new Vector2[36];
 
         // Writes a abstract about player's movement [OBS: MOVE CODE TO THE END?]
         File.WriteAllText(Application.dataPath + "/" + playerName + " - BorderLine.txt",
@@ -99,14 +99,21 @@ public class Functions : MonoBehaviour {
 
         // Reset movement space variables
         min = max = center = bases = Vector2.zero;
-        Vector2 polar, offset;
-
+        
+        // For each player's movement (less header)
         for (int i = 1; i < movementsText.Length; i++)
         {
             try
             {
+                // Break string into fields (0 - Time, 1 - X Pos, 2 - Y Pos, etc...)
+                // See dissertation's subseccion 5.2.2 for movement space study
                 string[] columns = movementsText[i].Split(tab);
+
+                // Transform movement into vector (converted to degrees)
                 movements[i - 1] = (new Vector2(float.Parse(columns[1]), float.Parse(columns[2]))) * Mathf.Rad2Deg;
+
+                // Verifies max and min movements to parametrize ellipse
+                // Used in function 5.7 (dissertation)
                 if (movements[i - 1].x < min.x)
                     min.x = movements[i - 1].x;
                 if (movements[i - 1].y < min.y)
@@ -121,13 +128,17 @@ public class Functions : MonoBehaviour {
                 Debug.Log(e);
             }
         }
-        movementsText = new string[0];
+        // movementsText = new string[0]; // OBS: Verify if it is necessary
 
+        // Parametrize ellipse (Function 5.7 in dissertation)
         center = (max + min) / 2f;
         bases = (max - min) / 2f;
+
+        // Centralize all borderline at ellipse center
         for (int i = 0; i < borderLine.Length; i++)
             borderLine[i] = center;
 
+        // Writes borderline header (OBS: Improve writers)
         File.AppendAllText(Application.dataPath + "/" + playerName + " - BorderLine.txt",
             "Border Calculated" + "\t" +
             Environment.NewLine +
@@ -154,26 +165,42 @@ public class Functions : MonoBehaviour {
             Environment.NewLine
             , Encoding.UTF8);
 
+        // For each movement vector...
         for (int i = 0; i < movements.Length; i++)
         {
+            // Calculates offset movement from ellipse center
             offset = movements[i] - center;
 
+            // Calculates polar coordinate alpha of this offset
             float ang = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+            // Adjust angle between 0 (included) and 360 (excluded) degrees 
             while (ang < 0f)
                 ang += 360f;
+
+            // Rounds polar coordinate alpha to borderline resolution (36 elements)
             int i_ang = Mathf.RoundToInt(ang / 10f);
+
+            // Adjust index between 0 (included) and 36 (excluded) degrees 
             if (i_ang >= 36)
                 i_ang -= 36;
             if (i_ang < 0)
                 i_ang += 36;
 
+            // Defines offset polar coordinate r is greater than current borderline
             polar = new Vector2(offset.magnitude, ang);
+
+            // Verifies if 
             if (polar.x > borderLine[i_ang].x)
                 borderLine[i_ang] = polar;
         }
-        movements = new Vector2[0];
 
+        // movements = new Vector2[0]; // OBS: Verify if is necessary
+
+        // For each borderline angle (resolution)...
         for (int i = 0; i < borderLine.Length; i++)
+        {
+            // Writes in borderline file
             File.AppendAllText(Application.dataPath + "/" + playerName + " - BorderLine.txt",
                     i + "\t" +
                     borderLine[i].x + "\t" +
@@ -184,5 +211,6 @@ public class Functions : MonoBehaviour {
                     (bases.y * Mathf.Sin(10f * i * Mathf.Deg2Rad) + center.y) + "\t" +
                     Environment.NewLine
                 , Encoding.UTF8);
+        }
     }
 }
