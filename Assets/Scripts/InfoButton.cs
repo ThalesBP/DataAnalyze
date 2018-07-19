e9a8a2741a4f9912b740ca573de26af98d45fd24
larsilveira@hotmail.com <larsilveira@hotmail.com>
2018-07-19 09:33:09 -0300

    Iniciando implementação do InfoButton

---
+++ Assets/Scripts/InfoButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoButton : MonoBehaviour {
+    public string playerName;
+
+    [SerializeField]
+    private Button infoButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        infoButton.onClick.AddListener(delegate
+        {
+            ShowInfo();
+        });
+    }
+	
+    void ShowInfo()
+    {
+        Debug.Log(playerName);
+    }
+}
--- Assets/Scripts/InterfaceManager.cs
+++ Assets/Scripts/InterfaceManager.cs
@@ -2,37 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 using System;
 using System.IO;
 using System.Text;
 
-public class InterfaceManager : Analyze {
+public class InterfaceManager : MonoBehaviour
+{
 
     public GameObject templateToggle;
     public GameObject toggleParent;
 
-  /*  public string playerName;
-    private string[] movementsText;*/
-  
+    public InterfaceManager interfaceManager;
+
+    public Button buttonAux;
+
+    private string[] playersText;
 
     // Use this for initialization
     void Start () {
-
+        playersText = File.ReadAllLines(Application.dataPath + "/Choices/Players.txt", Encoding.UTF8);
+        Debug.Log(playersText.Length);
       
-        for (int n = 0; n< 6; n++)
+        for (int n = 0; n< playersText.Length; n++)
         {
             
             GameObject toggleAux;
             Text textAux;
-                     
+            InfoButton infoButtonAux;          
+                 
             toggleAux = Instantiate(templateToggle, toggleParent.transform);
-            toggleAux.name = "Pacient_" + (n + 1).ToString() ;
+            toggleAux.name =  playersText[n]  ;
+
+            buttonAux = toggleAux.GetComponentInChildren<Button>();
+            buttonAux.name = "Button_" + playersText[n];
 
             textAux = toggleAux.GetComponentInChildren<Text>();
-            textAux.text = "Pacient_" + (n + 1).ToString();
+            textAux.text =  playersText[n];
+
+            infoButtonAux = toggleAux.GetComponent<InfoButton>();
+            infoButtonAux.playerName = playersText[n];
+    
         }
 
-       
+       /* if (interfaceManager.buttonAux.IsActive())
+        {
+            
+        }*/
     }
 	
 	// Update is called once per frame
